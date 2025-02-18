652df2d6f97855245ec4805466cd8c87bc3af931
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-02-18 07:48:00 +0600

    text

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'section1/basic flutter program/second_app.dart';
+import 'section1/text/text.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SecondApp(),
+      home: TextApp(),
     );
   }
 }
