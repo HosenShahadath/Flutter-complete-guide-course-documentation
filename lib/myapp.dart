6264817d27621b84daaf29335787984ab0210a39
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-02-25 06:32:26 +0600

    cached image

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section3/profile%20app.dart';
+import 'package:flutter_framework_practice/section3/cached%20image.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProfileApp(),
+      home: CachedImage(),
     );
   }
 }
