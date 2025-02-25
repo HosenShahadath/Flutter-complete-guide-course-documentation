b859cef6ca3ace6f83d4cd31b70970c9603f2bf1
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-02-25 06:50:32 +0600

    cached image

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section3/cached%20image.dart';
+import 'package:flutter_framework_practice/section3/fonts.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CachedImage(),
+      home: FontApp(),
     );
   }
 }
