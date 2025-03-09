c80c92939bbf54cab7309e6d6b723cb8190df313
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-09 10:17:53 +0600

    google fonts

---
+++ lib/section3/google fonts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GoogleFonts extends StatelessWidget {
+  const GoogleFonts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(
+        'This is Google Fonts',
+      ),
+    );
+  }
+}
