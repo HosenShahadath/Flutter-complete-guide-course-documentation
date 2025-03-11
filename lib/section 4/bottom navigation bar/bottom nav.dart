25a3e97e1c39e75cc59a9e2a59567a4ccf263487
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-11 09:35:18 +0600

    bottom nav

--- lib/section 4/bottom navigation bar/bottom nav.dart
+++ lib/section 4/bottom navigation bar/bottom nav.dart
@@ -7,19 +7,57 @@ class BottomNav extends StatefulWidget {
 
 class _BottomNavState extends State<BottomNav> {
 
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = const [
+    Text('Home'),
+    Text('Categories'),
+    Text('Cart'),
+    Text('Profile'),
+  ];
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'HOME'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
-        ],
-      ),
+     appBar: AppBar(
+       title: const Text('Ecomerce App', style: TextStyle(color: Colors.white),
+       ),
+       backgroundColor: Colors.indigo,
+     ),
+     body: Center(
+       child: _widgetOptions.elementAt(_selectedIndex),
+     ),
+     bottomNavigationBar: BottomNavigationBar(
+         type: BottomNavigationBarType.fixed,
+         backgroundColor: Colors.indigo,
+         selectedItemColor: Colors.amber,
+         unselectedItemColor: Colors.white,
+         items: const [
+           BottomNavigationBarItem(
+               icon: Icon(Icons.home),
+             label: 'Home'
+           ),
+           BottomNavigationBarItem(icon: Icon(Icons.category),
+           label: 'Categories',
+           ),
+           BottomNavigationBarItem(
+             icon: Icon(Icons.shopping_cart),
+             label: 'Cart',
+           ),
+           BottomNavigationBarItem(
+             icon: Icon(Icons.person),
+             label: 'Profile',
+           ),
+         ],
+       currentIndex: _selectedIndex,
+       onTap: _onItemTapped,
+     ),
     );
   }
 }
