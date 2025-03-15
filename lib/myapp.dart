b0baa22a281a690d5991e0fec0dd2a37d82c67c4
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-15 06:12:43 +0600

    switch

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section%206/checkbox/checkbox%204.dart';
-import 'package:flutter_framework_practice/section%206/radio%20button/radio%20button.dart';
+import 'package:flutter_framework_practice/section%206/switch/switch.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RadioButtonApp(),
+      home: SwitchApp(),
     );
   }
 }
---
+++ lib/section 6/switch/switch.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SwitchApp extends StatefulWidget {
+  const SwitchApp({super.key});
+
+  @override
+  State<SwitchApp> createState() => _SwitchAppState();
+}
+
+class _SwitchAppState extends State<SwitchApp> {
+  bool isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Switch(value: isSwitched,
+              onChanged: (value){
+              setState(() {
+                isSwitched = value;
+              });
+              }),
+          Text('Switch is ${isSwitched ? "ON" : 'OFF'}', style: TextStyle(
+            fontSize: 20,
+          ),)
+        ],
+      ),
+    );
+  }
+}
