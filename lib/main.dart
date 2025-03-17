f993ab24ccb1e7bd5ef65375388b8934df28d751
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-17 09:13:42 +0600

    pass data with named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_framework_practice/section%207/pass%20data%20to%20other%20screen/pass%20data%203.dart';
 import 'myapp.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(MyApp2());
 }
--- lib/section 7/pass data to other screen/pass data 3.dart
+++ lib/section 7/pass data to other screen/pass data 3.dart
@@ -1,66 +1,63 @@
 import 'package:flutter/material.dart';
 
-class Product{
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Named Route Navigation',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/product': (context) => ProductScreen(),
+      },
+    );
+  }
+}
+
+class Product {
   final String name;
   final double price;
 
   Product({required this.name, required this.price});
 }
 
-class MyApp2 extends StatelessWidget {
-  const MyApp2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Named Route Navigation',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/second': (context) => SecondScreen(),
-      },
-    );
-  }
-}
-
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
   final Product product = Product(name: 'Laptop', price: 1000);
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
       body: Center(
         child: ElevatedButton(
+          child: Text('View Product'),
           onPressed: () {
-            Navigator.pushNamed(context, '/second',
-            arguments: product,
-            );
+            Navigator.pushNamed(context, '/product', arguments: product);
           },
-          child: const Text('Go to second screen'),
         ),
       ),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
-
+class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final String args = ModalRoute.of(context)?.settings.arguments as String;
+    // Receiving the product object passed as an argument
+    final Product product = ModalRoute.of(context)!.settings.arguments as Product;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second  Screen'),
-      ),
+      appBar: AppBar(title: Text(product.name)),
       body: Center(
-        child: Text(args),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Name: ${product.name}'),
+            Text('Price: \$${product.price}'),
+          ],
+        ),
       ),
     );
   }
