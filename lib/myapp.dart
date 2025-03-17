4553fa2d56394a6484a57ea80557dec05c846499
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-17 07:49:00 +0600

    pass data

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section%207/move%20to%20another%20screen/move%20to%20screen%202.dart';
+import 'package:flutter_framework_practice/section%207/pass%20data%20to%20other%20screen/pass%20%20data%201.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomeScreenApp(),
     );
   }
 }
---
+++ lib/section 7/pass data to other screen/pass  data 1.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenApp extends StatelessWidget {
+  HomeScreenApp({super.key});
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Greet App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(hintText: 'Enter your Name'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => GreetScreen(name: _controller.text),
+                  ),
+                );
+              },
+              child: Text('Greet'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GreetScreen extends StatelessWidget {
+  const GreetScreen({super.key, required this.name});
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Greet Screen')),
+      body: Center(child: Text('Hello, $name!')),
+    );
+  }
+}
