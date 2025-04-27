30500a63cf4b903a7c9dd3e0b91fd301087e3472
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-04-27 10:31:17 +0600

    listview

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section1/counter%20app/counter_app_3.dart';
+import 'package:flutter_framework_practice/section2/Listview/todo_list_app.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterApp3(),
+      home: TodoListApp(),
     );
   }
 }
---
+++ lib/section2/Listview/todo_list_app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.sports_gymnastics),
+            title: Text('Go to Gym'),
+            subtitle: Text('Go to gym at 6.00 AM'),
+            trailing: Icon(Icons.timelapse),
+          ),
+          ListTile(
+            leading: Icon(Icons.sports_gymnastics),
+            title: Text('Go to Gym'),
+            subtitle: Text('Go to gym at 6.00 AM'),
+            trailing: Icon(Icons.timelapse),
+          ),
+          ListTile(
+            leading: Icon(Icons.sports_gymnastics),
+            title: Text('Go to Gym'),
+            subtitle: Text('Go to gym at 6.00 AM'),
+            trailing: Icon(Icons.timelapse),
+          ),
+          ListTile(
+            leading: Icon(Icons.sports_gymnastics),
+            title: Text('Go to Gym'),
+            subtitle: Text('Go to gym at 6.00 AM'),
+            trailing: Icon(Icons.timelapse),
+          ),
+        ],
+      ),
+    );
+  }
+}
