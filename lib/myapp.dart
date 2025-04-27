bdfc5836b09bead34d6c25603994474c6f743099
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-04-27 11:06:15 +0600

    social media

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_framework_practice/section2/Gridview/dashboard_app.dart';
 import 'package:flutter_framework_practice/section2/Gridview/photo_gallery_app.dart';
 import 'package:flutter_framework_practice/section2/Listview%20Builder/simple_listview_builder.dart';
+import 'package:flutter_framework_practice/section2/Listview%20Builder/social_media.dart';
 import 'package:flutter_framework_practice/section2/Listview%20Builder/todo_app3.dart';
 import 'package:flutter_framework_practice/section2/Listview%20Builder/todo_app_4.dart';
 import 'package:flutter_framework_practice/section2/Listview/Image_gallary_app.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TodoApp4(),
+      home: SocialMedia(),
     );
   }
 }
---
+++ lib/section2/Listview Builder/social_media.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class SocialMedia extends StatelessWidget {
+  SocialMedia({super.key});
+
+  List<Post> posts = [
+    Post(
+      username: 'John Doe',
+      userImageUrl: 'https://picsum.photos/250?image=237',
+      timestamp: '2h',
+      contentText: 'Enjoying the beautiful sunset at the beach!',
+      contentImageUrl: 'https://picsum.photos/250?image=51',
+    ),
+    Post(
+      username: 'Mark Doe',
+      userImageUrl: 'https://picsum.photos/250?image=238',
+      timestamp: '1d',
+      contentText: 'Just got back from a fun vacation in the mountains.',
+      contentImageUrl: 'https://picsum.photos/250?image=52',
+    ),
+    Post(
+      username: 'John Doe',
+      userImageUrl: 'https://picsum.photos/250?image=237',
+      timestamp: '2h',
+      contentText: 'Enjoying the beautiful sunset at the beach!',
+      contentImageUrl: 'https://picsum.photos/250?image=51',
+    ),
+    Post(
+      username: 'Mark Doe',
+      userImageUrl: 'https://picsum.photos/250?image=238',
+      timestamp: '1d',
+      contentText: 'Just got back from a fun vacation in the mountains.',
+      contentImageUrl: 'https://picsum.photos/250?image=52',
+    ),
+    // Add more sample posts
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index){
+        return Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(posts[index].userImageUrl),
+                ),
+                title: Text(posts[index].username),
+                subtitle: Text(posts[index].timestamp),
+              ),
+              Padding(padding: EdgeInsets.all(8),
+              child: Text(posts[index].contentText),
+              ),
+              Image.network(posts[index].contentImageUrl)
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class Post {
+  final String username;
+  final String userImageUrl;
+  final String timestamp;
+  final String contentText;
+  final String contentImageUrl;
+
+  Post({
+    required this.username,
+    required this.userImageUrl,
+    required this.timestamp,
+    required this.contentText,
+    required this.contentImageUrl,
+  });
+}
