25cef17d79876f597fbd492450fa534da81b9ff2
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-04-30 12:22:51 +0600

    elevated button

--- lib/section 5/elevated button/elevated_button_screen.dart
+++ lib/section 5/elevated button/elevated_button_screen.dart
@@ -9,7 +9,31 @@ class ElevatedButtonScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: Column(
-          children: [ElevatedButton(onPressed: () {}, child: Text('Press Me'))],
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text('Press Me')),
+            SizedBox(height: 16),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text('Press Me'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                  )
+                ),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 20),
+                ),
+              ),
+              child: Text('Custom Style'),
+            )
+          ],
         ),
       ),
     );
