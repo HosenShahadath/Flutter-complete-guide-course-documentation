78561ab8f008ed5381e66e53b2ce8cd3ee834403
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-05-03 06:34:03 +0600

    navigation

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section%206/form%20validation/form_validator_screen.dart';
+import 'package:flutter_framework_practice/section%207/move%20to%20another%20screen/move_to_another_screen.dart';
 
 
 class MyApp extends StatelessWidget {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FormValidatorScreen(),
+      home: MoveToAnotherScreen(),
     );
   }
 }
---
+++ lib/section 7/move to another screen/move_to_another_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MoveToAnotherScreen extends StatelessWidget {
+  const MoveToAnotherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+          child: Text('Move to another screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: Center(
+        child: ElevatedButton(onPressed: () {
+          Navigator.pop(context);
+        }, child: const Text('Go back')),
+      ),
+    );
+  }
+}
