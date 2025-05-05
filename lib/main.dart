4e43a7faa1a727455f37285cdea7a7bca63705e5
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-05-05 06:09:40 +0600

    shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_framework_practice/section%209/shared%20preferencs/shared_preferences_app.dart';
 import 'myapp.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferencesHelper.init();
   runApp(MyApp());
 }
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_framework_practice/section%207/TabBar/tabbar_screen.dart';
+import 'package:flutter_framework_practice/section%209/shared_preferences_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TabbarScreen(),
+      home: SharedPreferencesScreen(),
     );
   }
 }
